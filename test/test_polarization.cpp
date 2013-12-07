a346f8ea427a2130443a802ef372f0afd3ba7bea
Léo Cavaillé <leo@cavaille.net>
2013-12-07 12:34:20 +0100

    Use the new-style decode rather than as<> for Polarization

--- test/test_polarization.cpp
+++ test/test_polarization.cpp
@@ -4,19 +4,22 @@
 
 TEST(PolarizationTest, VerticalYamlParse) {
   YAML::Node doc = YAML::LoadFile("yaml/polarization/vertical.yaml");
-  PolarizationPtr p = doc["polarization"].as<PolarizationPtr>();
+  PolarizationPtr p = std::make_shared<Polarization>();
+  YAML::convert<PolarizationPtr>::decode(doc["polarization"], p);
 
   ASSERT_EQ(*p, Polarization::vertical);
 }
 
 TEST(PolarizationTest, HorizontalYamlParse) {
   YAML::Node doc = YAML::LoadFile("yaml/polarization/horizontal.yaml");
-  PolarizationPtr p = doc["polarization"].as<PolarizationPtr>();
+  PolarizationPtr p = std::make_shared<Polarization>();
+  YAML::convert<PolarizationPtr>::decode(doc["polarization"], p);
 
   ASSERT_EQ(*p, Polarization::horizontal);
 }
 
 TEST(PolarizationTest, InvalidYamlParse) {
   YAML::Node doc = YAML::LoadFile("yaml/polarization/bad.yaml");
-  ASSERT_ANY_THROW(doc["polarization"].as<PolarizationPtr>());
+  PolarizationPtr p = std::make_shared<Polarization>();
+  ASSERT_ANY_THROW(YAML::convert<PolarizationPtr>::decode(doc["polarization"], p));
 }
