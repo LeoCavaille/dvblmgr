e0926206d305767df43b5a20eb3130ebd4eca8c7
Benjamin Fernandes <lotharsee@gmail.com>
2013-12-08 16:55:53 +0100

    Add and use Multiplex::getType, add multiplex tests

--- src/multiplex.cpp
+++ src/multiplex.cpp
@@ -1,1 +1,17 @@
 #include "multiplex.h"
+
+std::string Multiplex::getType(const YAML::Node& node) {
+    if(!node["type"]) {
+        return "UNKNOWN";
+    }
+
+    std::string type = node["type"].as<std::string>();
+    std::transform(type.begin(), type.end(),type.begin(), ::toupper);
+
+    if(type == "TNT" || type == "SAT") {
+        return type;
+    }
+    else{
+        return "UNKNOWN";
+    }
+}
--- test/test_multiplex.cpp
+++ test/test_multiplex.cpp
@@ -1,12 +1,26 @@
 #include "multiplex.h"
+#include "multiplexsat.h"
+#include "multiplextnt.h"
 #include <fstream>
 
 #include "gtest/gtest.h"
 
 TEST(MultiplexTest, YAMLGoodSATParsing) {
   YAML::Node doc = YAML::LoadFile("yaml/multiplex/good_dvbs.yaml");
+  std::string type = Multiplex::getType(doc);
+
+  ASSERT_EQ(type, "SAT");
+
+  // MultiplexSatPtr m = std::make_shared<MultiplexSat>();
+  // ASSERT_TRUE(YAML::convert<MultiplexSatPtr>::decode(doc, m));
 }
 
 TEST(MultiplexTest, YAMLGoodTNTParsing) {
   YAML::Node doc = YAML::LoadFile("yaml/multiplex/good_dvbt.yaml");
+  std::string type = Multiplex::getType(doc);
+
+  ASSERT_EQ(type, "TNT");
+
+  MultiplexTntPtr m = std::make_shared<MultiplexTnt>();
+  ASSERT_TRUE(YAML::convert<MultiplexTntPtr>::decode(doc, m));
 }
