fb8b465cf82a4a26ebf3f7ca5a945fde4d09e3f0
Benjamin Fernandes <lotharsee@gmail.com>
2013-12-29 18:07:14 +0100

    Add broadcast list generation logic and tests

---
+++ dvblmgr/test/test_broadcastlistgenerator.cpp
@@ -0,0 +1,15 @@
+#include "configuration.hpp"
+#include "broadcastlistgenerator.hpp"
+
+#include "gtest/gtest.h"
+// #include <iostream>
+
+TEST(BroadcastlistGeneratorTest, GenerateAssignementConfig) {
+  ConfigurationPtr cPtr = std::make_shared<Configuration>("yaml/broadcastlistgenerator/good.yaml");
+  cPtr->load();
+
+  BroadcastListGenerator generator(cPtr);
+  generator.setOffline(true);
+  Assignement assignement = generator.generateAssignement();
+  ASSERT_EQ(assignement.size(), 5);
+}
